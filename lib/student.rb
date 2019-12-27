68ea73a47ebf272d8cc685b83091a2c1dd7ab14a
Richard Lee <richlee81192@gmail.com>
2019-12-27 23:25:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,7 @@ class Student
   end
   
   def self.create_table
-    sql = <<- SQL
+    sql = <<-SQL
       CREATE TABLE IF NOT EXISTS songs (
         id INTEGER PRIMARY KEY,
         name TEXT,
@@ -20,20 +20,27 @@ class Student
   end
   
   def self.drop_table
-    sql = <<- SQL (
+    sql = <<-SQL 
       DROP TABLE songs
-      )
       SQL
     DB[:conn].execute(sql)
   end
   
   def save
-    sql = SQL (
-      INSERT 
-      )
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES(?, ?)
+      SQL
+      
+      DB[:conn].execute(sql, self.name, self.album)
     
-    
-    
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]  
+  end
+  
+  def self.create(:name, :grade)
+    song = Song.new(name, grade)
+    song.save
+    song
   end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
